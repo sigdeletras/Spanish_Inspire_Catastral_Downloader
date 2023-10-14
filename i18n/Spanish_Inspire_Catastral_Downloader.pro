52c9b3dcb22aea534e5c8908e5e1c452c3b0b15b
Patricio Soriano <pasoriano@gmail.com>
2023-10-14 17:36:36 +0200

    Actualizados archivos de traducción al español

--- i18n/Spanish_Inspire_Catastral_Downloader.pro
+++ i18n/Spanish_Inspire_Catastral_Downloader.pro
@@ -3,4 +3,4 @@ FORMS = ../Spanish_Inspire_Catastral_Downloader_dialog_base.ui
 SOURCES = ../Spanish_Inspire_Catastral_Downloader.py \
 ../Spanish_Inspire_Catastral_Downloader_dialog.py
 
-TRANSLATIONS = Spanish_Inspire_Catastral_es.ts
+TRANSLATIONS = Spanish_Inspire_Catastral_Downloader_es.ts
--- i18n/Spanish_Inspire_Catastral_Downloader_es.ts
+++ i18n/Spanish_Inspire_Catastral_Downloader_es.ts
@@ -4,9 +4,9 @@
 <context>
     <name>Spanish_Inspire_Catastral_Downloader</name>
     <message>
-        <location filename="../Spanish_Inspire_Catastral_Downloader.py" line="192"/>
+        <location filename="../Spanish_Inspire_Catastral_Downloader.py" line="217"/>
         <source>&amp;Spanish Inspire Catastral Downloader</source>
-        <translation>Descarga Catastro Inspire</translation>
+        <translation>&amp;Descargada Catastro Inspire de España</translation>
     </message>
 </context>
 <context>
@@ -14,52 +14,52 @@
     <message>
         <location filename="../Spanish_Inspire_Catastral_Downloader_dialog_base.ui" line="14"/>
         <source>Spanish Inspire Catastral Downloader</source>
-        <translation>Descarga Catastro Inspire</translation>
+        <translation>Descargada Catastro Inspire de España</translation>
     </message>
     <message>
-        <location filename="../Spanish_Inspire_Catastral_Downloader_dialog_base.ui" line="52"/>
-        <source>...</source>
-        <translation>...</translation>
-    </message>
-    <message>
-        <location filename="../Spanish_Inspire_Catastral_Downloader_dialog_base.ui" line="85"/>
-        <source>Add layers to the QGIS project</source>
-        <translation>Cargar capas en el proyecto QGIS</translation>
-    </message>
-    <message>
-        <location filename="../Spanish_Inspire_Catastral_Downloader_dialog_base.ui" line="101"/>
-        <source>Cadastral Parcels</source>
-        <translation>Parcelas Catastrales</translation>
-    </message>
-    <message>
-        <location filename="../Spanish_Inspire_Catastral_Downloader_dialog_base.ui" line="108"/>
-        <source>Buildings</source>
-        <translation>Edificios</translation>
-    </message>
-    <message>
-        <location filename="../Spanish_Inspire_Catastral_Downloader_dialog_base.ui" line="115"/>
-        <source>Addresses</source>
-        <translation>Direcciones</translation>
-    </message>
-    <message>
-        <location filename="../Spanish_Inspire_Catastral_Downloader_dialog_base.ui" line="131"/>
+        <location filename="../Spanish_Inspire_Catastral_Downloader_dialog_base.ui" line="28"/>
         <source>Province</source>
         <translation>Provincia</translation>
     </message>
     <message>
-        <location filename="../Spanish_Inspire_Catastral_Downloader_dialog_base.ui" line="144"/>
+        <location filename="../Spanish_Inspire_Catastral_Downloader_dialog_base.ui" line="41"/>
         <source>Municipality</source>
         <translation>Municipio</translation>
     </message>
     <message>
-        <location filename="../Spanish_Inspire_Catastral_Downloader_dialog_base.ui" line="157"/>
+        <location filename="../Spanish_Inspire_Catastral_Downloader_dialog_base.ui" line="81"/>
+        <source>Buildings</source>
+        <translation>Construcciones</translation>
+    </message>
+    <message>
+        <location filename="../Spanish_Inspire_Catastral_Downloader_dialog_base.ui" line="94"/>
+        <source>Addresses</source>
+        <translation>Direcciones</translation>
+    </message>
+    <message>
+        <location filename="../Spanish_Inspire_Catastral_Downloader_dialog_base.ui" line="107"/>
+        <source>Cadastral Parcels</source>
+        <translation>Parcelas catastrales</translation>
+    </message>
+    <message>
+        <location filename="../Spanish_Inspire_Catastral_Downloader_dialog_base.ui" line="116"/>
         <source>Download folder</source>
         <translation>Carpeta de descarga</translation>
     </message>
     <message>
-        <location filename="../Spanish_Inspire_Catastral_Downloader_dialog_base.ui" line="170"/>
-        <source>Run</source>
-        <translation>Ejecutar</translation>
+        <location filename="../Spanish_Inspire_Catastral_Downloader_dialog_base.ui" line="141"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="../Spanish_Inspire_Catastral_Downloader_dialog_base.ui" line="150"/>
+        <source>Download cadastral data</source>
+        <translation>Descargar datos catastrales</translation>
+    </message>
+    <message>
+        <location filename="../Spanish_Inspire_Catastral_Downloader_dialog_base.ui" line="157"/>
+        <source>Add layers to the QGIS project</source>
+        <translation>Añadir capas al proyecto QGIS</translation>
     </message>
 </context>
 </TS>
